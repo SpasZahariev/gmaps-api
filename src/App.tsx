5c4b49ebcada8fe779e7bcf6c28785efc188dfe1
SpasZahariev <spaszahariev54@gmail.com>
2019-08-25 11:53:35 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 
-const App: React.FC = () => {
+//to get the script working we need to wrap this in other funs
+function Map() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 42.6977, lng: 23.3219 }}
+    />
   );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+class App extends React.Component {
+  render() {
+    return (
+      <div id="map">
+        <WrappedMap
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_MAP_API}`}
+          loadingElement={<div style={{ height: "100%" }} />}
+          containerElement={<div style={{ height: "100%" }} />}
+          mapElement={<div style={{ height: "100%" }} />}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
